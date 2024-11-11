1509d16f52fd0df0d1988930660a84128ef95fdc
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-11-11 17:09:58 +0800

    [3] Add animation to hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,37 @@ import { useState } from "react";
 import headphone1 from "../assets/headphone.png";
 import headphone2 from "../assets/headphone2.png";
 import headphone3 from "../assets/headphone3.png";
+import { UpdateFollower } from "react-mouse-follower";
+import { AnimatePresence, easeInOut, motion } from "framer-motion";
+
+const fadeUp = (delay) => {
+  return {
+    hidden: {
+      opacaity: 0,
+      y: 100,
+      scale: 0.5,
+    },
+    show: {
+      opacaity: 1,
+      y: 0,
+      scale: 1,
+      transition: {
+        duration: 0.5,
+        delay: delay,
+        ease: easeInOut,
+      },
+    },
+    exit: {
+      opacaity: 0,
+      y: 1000,
+      scale: 0.5,
+      transition: {
+        duration: 0.2,
+        ease: easeInOut,
+      },
+    },
+  };
+};
 
 const headphoneData = [
   {
@@ -40,6 +71,10 @@ const headphoneData = [
 const Hero = () => {
   const [activeData, setActiveData] = useState(headphoneData[0]);
 
+  const handleActiveData = (data) => {
+    setActiveData(data);
+  };
+
   return (
     <>
       <section className="bg-brandDark text-white font-varela">
@@ -47,18 +82,71 @@ const Hero = () => {
           {/* Headphone info */}
           <div className="flex flex-col justify-center py-14 md:py-0 xl:max-w-[500px]">
             <div className="space-y-5 text-center md:text-left">
-              <h1 className="text-3xl lg:text-6xl font-bold font-varela">
-                {activeData.title}
-              </h1>
-              <p className="text-sm leading-loose text-white/80">
-                {activeData.description}
-              </p>
-              <button
-                style={{ backgroundColor: activeData.bgColor }}
-                className="px-4 py-2 inline-block font-normal rounded-sm"
-              >
-                Buy and Listen
-              </button>
+              <AnimatePresence mode="wait">
+                <UpdateFollower
+                  mouseOptions={{
+                    backgroundColor: "white",
+                    zIndex: 9999,
+                    followSpeed: 0.5,
+                    rotate: -720,
+                    mixBlendMode: "difference",
+                    scale: 10,
+                  }}
+                >
+                  <motion.h1
+                    key={activeData.id}
+                    variants={fadeUp(0.2)}
+                    initial="hidden"
+                    animate="show"
+                    exit="exit"
+                    className="text-3xl lg:text-6xl font-bold font-varela"
+                  >
+                    {activeData.title}
+                  </motion.h1>
+                </UpdateFollower>
+              </AnimatePresence>
+
+              <AnimatePresence mode="wait">
+                <motion.p
+                  key={activeData.id}
+                  variants={fadeUp(0.3)}
+                  initial="hidden"
+                  animate="show"
+                  exit="exit"
+                  className="text-sm leading-loose text-white/80"
+                >
+                  {activeData.description}
+                </motion.p>
+              </AnimatePresence>
+
+              <AnimatePresence mode="wait">
+                <UpdateFollower
+                  mouseOptions={{
+                    backgroundColor: activeData.bgColor,
+                    zIndex: 9999,
+                    followSpeed: 0.5,
+                    rotate: -720,
+                    scale: 10,
+                    backgroundElement: (
+                      <div>
+                        <img src={activeData.image} />
+                      </div>
+                    ),
+                  }}
+                >
+                  <motion.button
+                    key={activeData.id}
+                    variants={fadeUp(0.3)}
+                    initial="hidden"
+                    animate="show"
+                    exit="exit"
+                    style={{ backgroundColor: activeData.bgColor }}
+                    className="px-4 py-2 inline-block font-normal rounded-sm"
+                  >
+                    Buy and Listen
+                  </motion.button>
+                </UpdateFollower>
+              </AnimatePresence>
 
               {/* Headphone List Separator */}
               <div className="flex items-center justify-center md:justify-start gap-4 !mt-24">
@@ -72,7 +160,8 @@ const Hero = () => {
                 {headphoneData.map((item) => {
                   return (
                     <div
-                      key={item}
+                      key={item.id}
+                      onClick={() => handleActiveData(item)}
                       className="grid grid-cols-2 place-items-center cursor-pointer"
                     >
                       <div>
@@ -92,14 +181,27 @@ const Hero = () => {
           </div>
           {/* Hero Image */}
           <div className="flex flex-col justify-end items-center">
-            <img
-              src={activeData.image}
-              alt=""
-              className="w-[300px] md:w-[400px] xl:w-[550px]"
-            />
+            <AnimatePresence>
+              <motion.img
+                key={activeData.id}
+                variants={fadeUp(0.4)}
+                initial={{ opacity: 0, scale: 0.9, y: 100 }}
+                animate={{ opacity: 1, scale: 1, y: 0 }}
+                exit={{
+                  opacity: 0,
+                  scale: 0.9,
+                  y: 100,
+                  transition: { duration: 0.2 },
+                }}
+                transition={{ duration: 0.5, delay: 0.2, ease: easeInOut }}
+                src={activeData.image}
+                alt=""
+                className="w-[300px] md:w-[400px] xl:w-[550px]"
+              />
+            </AnimatePresence>
           </div>
           {/* WhatsApp Icon  */}
-          <div>
+          <div className="text-3xl text-white fixed bottom-10 right-10 hover:rotate-[360deg] duration-500 z-[99999] mix-blend-difference">
             <a href="">
               <FaWhatsapp />
             </a>
